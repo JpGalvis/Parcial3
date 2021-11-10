a8a974f56f7b8f21f07425ea09aa30d1af3408c3
jgalvissaavedra <jgalvissaavedra@gmail.com>
2021-11-10 14:22:51 -0400

    models creado

---
+++ Almacen/Almacen/Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Almacen.Models
+{
+    public class Producto
+    {
+        [Key]
+        public string producto { get; set; }
+        [Display(Name = "Nombre del producto")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "El nombre debe contener entre 3 a 60 caracteres")]
+        [Required(ErrorMessage = "El nombre del producto es requerido")]
+        public string almacen { get; set; }
+        [Display(Name = "nombre o nro de almacen")]
+        public string informacion { get; set; }
+        [Url]
+        [Display(Name = "informacion de almacen")]
+        public string link { get; set; }
+
+    }
+}
